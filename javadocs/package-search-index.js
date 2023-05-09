40099960b5138f238a97cea7627ee8c9bc586ede
littleBitsman <adittyanischal@hotmail.com>
2023-05-09 07:23:48 -0400

    a

--- javadocs/package-search-index.js
+++ javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.littleBitsman.Color3"}];updateSearchResults();
--- javadocs/type-search-index.js
+++ javadocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Examples"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.littleBitsman.Color3","l":"Color3"}];updateSearchResults();
